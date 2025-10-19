dc2c1ced04017b5d4cb0812cc9e8c87a7b13f111
Lana <colossalblot@gmail.com>
2025-10-19 14:03:46 -0400

    Understanding puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main() {
+int main(void)
+{
 	char message[] = "Programming is like building a multilingual puzzle";
-	puts (message);
-	return 0;
+
+	puts(message);
+
+	return (0);
 }
